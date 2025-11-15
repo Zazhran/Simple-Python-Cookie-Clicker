79da081b871ca34ff5708bb6d2433b16ef4c525c
Zazhran <zazhran03@gmail.com>
2025-11-15 21:22:54 +0700

    Add cookie clicker game functionality
    Implement basic cookie clicker game mechanics.

---
+++ cookie-clicker/python-cookie-clicker.py
@@ -0,0 +1,65 @@
+game = {
+      'cookies' : 0,  
+      'click_power' : 1,
+      'upgrade_cost' : 10
+}
+
+
+cookies_art = """
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣀⡴⠚⣉⡙⠲⠦⠤⠤⣤⡀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⢀⣴⠛⠉⠉⠀⣾⣷⣿⡆⠀⠀⠀⠐⠛⠿⢟⡲⢦⡀⠀⠀⠀⠀
+⠀⠀⠀⠀⣠⢞⣭⠎⠀⠀⠀⠀⠘⠛⠛⠀⠀⢀⡀⠀⠀⠀⠀⠈⠓⠿⣄⠀⠀⠀
+⠀⠀⠀⡜⣱⠋⠀⠀⣠⣤⢄⠀⠀⠀⠀⠀⠀⣿⡟⣆⠀⠀⠀⠀⠀⠀⠻⢷⡄⠀
+⠀⢀⣜⠜⠁⠀⠀⠀⢿⣿⣷⣵⠀⠀⠀⠀⠀⠿⠿⠿⠀⠀⣴⣶⣦⡀⠀⠰⣹⡆
+⢀⡞⠆⠀⣀⡀⠀⠀⠘⠛⠉⠁⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢿⣿⣶⠇⠀⢠⢻⡇
+⢸⠃⠘⣾⣏⡇⠀⠀⠀⠀⠀⠀⠀⡀⠀⠀⠀⠀⠀⠀⣠⣤⣤⡉⠁⠀⠀⠈⠫⣧
+⡸⡄⠀⠘⠟⠀⠀⠀⠀⠀⠀⣰⣿⣟⢧⠀⠀⠀⠀⠰⡿⣿⣿⢿⠀⠀⣰⣷⢡⢸
+⣿⡇⠀⠀⠀⣰⣿⡻⡆⠀⠀⠻⣿⣿⣟⠀⠀⠀⠀⠀⠉⠉⠉⠀⠀⠘⢿⡿⣸⡞
+⠹⣽⣤⣤⣤⣹⣿⡿⠇⠀⠀⠀⠀⠉⠁⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⡔⣽⠀
+⠀⠙⢻⡙⠟⣹⠟⢷⣶⣄⢀⣴⣶⣄⠀⠀⠀⠀⠀⢀⣤⡦⣄⠀⠀⢠⣾⢸⠏⠀
+⠀⠀⠘⠀⠀⠀⠀⠀⠈⢷⢼⣿⡿⡽⠀⠀⠀⠀⠀⠸⣿⣿⣾⠀⣼⡿⣣⠟⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⠀⢠⡾⣆⠑⠋⠀⢀⣀⠀⠀⠀⠀⠈⠈⢁⣴⢫⡿⠁⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⠀⠈⠙⣧⣄⡄⠴⣿⣶⣿⢀⣤⠶⣞⣋⣩⣵⠏⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⢺⣿⢯⣭⣭⣯⣯⣥⡵⠿⠟⠛⠉⠉⠀⠀⠀⠀⠀⠀⠀
+"""
+
+print(cookies_art)
+print("======COOKIE CLICKER ASELI KHAS PYTHON=====")
+print("1. Cookie Click! ")
+print("2. Upgrade!")
+print("3. Exit")
+        
+
+
+
+while True:
+        
+     
+        if game["cookies"] >= game["upgrade_cost"]:
+            print('You have enough cookies to upgrade :D')
+
+        choices = input("> ")
+        if choices == "1":
+            game["cookies"] += game["click_power"]
+            print(f'\n========={game["cookies"]}========')
+
+        elif choices == '2':
+              if game["cookies"] >= game["upgrade_cost"]:
+                    game["click_power"] += 1
+                    game["cookies"] -= 10
+                    game["upgrade_cost"] = game["upgrade_cost"] * 2
+                    print('You will gain +', game["click_power"])
+                    print('Upgrade price has been doubled : ', game["upgrade_cost"])
+
+              else:
+                    print("You dont have enough cookies yet! keep it up")
+        elif choices == '3':
+              print('See you later, Cookie heroes! :D')
+              break
+        else:
+              print("chill bruv")
+
+              
+
+
+      
